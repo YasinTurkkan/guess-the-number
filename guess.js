309e35982f207b19e89cb3da4b4b7af120975962
Yasin Turkkan <yasinturkkan@gmail.com>
2021-04-04 01:01:52 -0700

    initial commit

---
+++ guess.js
@@ -0,0 +1,48 @@
+
+const submitBtn = document.getElementById('submit');
+let ourRandomNumber = 0;
+let guessNumber = 0;
+
+function randomNumber() {
+  return Math.trunc(Math.random() * (100 - 1) + 1);
+}
+
+ourRandomNumber = randomNumber();
+
+const inputForm = document.getElementById("guessform");
+
+// button4.addEventListener('click', eventListener); //, { once: true });
+
+inputForm.addEventListener('submit', game);
+
+function game() {
+
+    guessNumber += 1;
+
+    const guessMessage = document.getElementById("guessnum");
+    guessMessage.innerHTML = `Number of attempts:<b>${guessNumber}</b>`;
+
+    event.preventDefault();
+    let playerNumber = document.getElementsByName("number")[0].value;
+
+    let message = "";
+
+      if (playerNumber == "") {
+        message = "You must enter a number";
+        setTimeout("location.reload(true);", 500);
+      } else if (playerNumber < ourRandomNumber) {
+        message = "GO UP";
+      } else if (playerNumber > ourRandomNumber) {
+        message = "GO DOWN";
+      } else {
+        message = "GOOD JOB. CONGURATULATIONS";
+        submitBtn.disabled = true;
+      }
+
+    const adviceMessage = document.getElementById("advice");
+    adviceMessage.innerHTML = `<b>${message}</b>`;
+
+    console.log(playerNumber);
+    console.log("Random number is:" + ourRandomNumber);
+
+}
